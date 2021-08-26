7709bef9d381b99ab395f35f66258f976cf04ac3
Moses Oyet David <trickmaxillar@ggmail.com>
2021-08-26 14:20:14 -0400

    Modify file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main -print exactly "and the piece of art isuseful"
+*- Dora Korpar 2015-10
+*followed by a new line, to the standard error
+*Return: Always 1 (success)
+*/
+int main(void)
+{
+write(2, "and the piece of art is useful\" - Dora Korpar, 2015-10\n", 59);
+return (1);
+}
