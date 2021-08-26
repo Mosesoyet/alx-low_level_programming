7d2f43aa0049f66e39d5fb03c5462862236593ee
Moses Oyet David <trickmaxillar@ggmail.com>
2021-08-26 14:08:32 -0400

    Modify file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,8 @@ int main(void)
 {
 printf("Size of char is: %d byte(s)\n", sizeof(char));
 printf("Size of int is: %d byte(s)\n", sizeof(int));
+printf("Size of long long int is: %d byte(s)\n", sizeof(long long int));
 printf("Size of long int is: %d byte(s)\n", sizeof(long int));
 printf("Size of float is: %d byte(s)\n", sizeof(float));
- return (0);
+return (0);
 }
