12b6796fa334531eb1b95bf362c917812816867a
Moses Oyet David <trickmaxillar@ggmail.com>
2021-08-26 13:37:21 -0400

    Modify file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *Program to print something
- *Always printf
- */
+*Program to print something
+*Always printf
+*/
 int main(void){
 printf("with proper grammer, but the outcome is a piece of art,");
- return (0);
+return (0);
 }
