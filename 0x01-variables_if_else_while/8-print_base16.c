97642670eefb2768436250c8bb747e56ca573049
Moses Oyet David <trickmaxillar@ggmail.com>
2021-08-27 11:39:12 -0400

    modify file

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h)
 /**
 *main- Main have loop to print out base16
 *Return: Return 0 success
@@ -12,10 +11,6 @@ int k;
 for (k = 0; k < 10; k++)
 {
 putchar((k % 10) + '0');
-}
-for (k = 'A'; k <+ F; k++)
-{
-putchar(tolower(k));
 }
 putchar('\n');
 return (0);
