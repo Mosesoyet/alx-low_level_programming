b78f0af0ab1ffd6c9035b5d3a4a6eb74070f1a25
Moses Oyet David <trickmaxillar@ggmail.com>
2021-08-27 13:41:19 -0400

    Modify file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main- Main contains nested loop
+*Return: Always 0 success
+*/
+
+
+int main(void)
+{
+int t;
+
+for (t = 48; t < 58; t++)
+{
+putchar(n);
+if (t != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
