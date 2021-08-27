834c5488afe4fcf3f0b1bbf302346894fdd13352
Moses Oyet David <trickmaxillar@ggmail.com>
2021-08-27 06:57:39 -0400

    Modify file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main- Main have code that out use putchar
+*Return: return 0 success
+*/
+
+int main(void)
+{
+int x;
+for (x = 'A'; x <= 'Z'; x++)
+{
+putchar(tolower(x));
+}
+for (x = 'a'; x <= 'z'; x++)
+{
+putchar(toupper(x));
+}
+return (0);
+}
