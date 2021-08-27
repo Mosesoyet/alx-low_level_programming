775d93de1f395f80f0f31f80a3adfe5b3d74acbe
Moses Oyet David <trickmaxillar@ggmail.com>
2021-08-27 05:10:32 -0400

    Modify file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main-Main should contain putchar and a variable
+*Return:return 0 success
+*/
+
+int main(void)
+{
+  int x;
+  for (x = 'A'; x <= 'Z'; x++)
+    {
+      putchar(tolower(x));
+      putchar(10);
+    }
+  return (0);
+}
