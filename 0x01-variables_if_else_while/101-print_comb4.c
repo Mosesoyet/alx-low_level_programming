8bac0e00af8c00ecbc4e2c2310aaa7a971f45213
Moses Oyet David <trickmaxillar@ggmail.com>
2021-08-28 04:08:13 -0400

    Add text in file

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,18 +6,29 @@
 */
 int main(void)
 {
-int x;
-for (x = 1; x < 10; x++)
+int n, m, l;
+
+for (n = 48; n < 58; n++)
 {
-putchar((x % 10) + '0');
-putchar((x / 10) + '0');
-putchar((x % 10) + '0');
-if (x != 9)
+for (m = 49; m < 58; m++)
+{
+for (l = 50; l < 58; l++)
+{
+if (l > m && m > n)
+{
+putchar(n);
+putchar(m);
+putchar(l);
+if (n != 55 || m != 56)
 {
 putchar(',');
 putchar(' ');
 }
 }
+}
+}
+}
+  
 putchar('\n');
 return (0);
 }
