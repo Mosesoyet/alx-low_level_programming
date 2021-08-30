fbe6cf514bd87a4691814ccb8b493f9c86fd9650
Moses Oyet David <trickmaxillar@ggmail.com>
2021-08-30 05:28:37 -0400

    Fix error

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,14 +8,14 @@
 int main(void)
 {
 int a, b;
-for (a = 0; a < 100; ++a)
+for (a = 0; a < 100; a++)
 {
-for (b = 0; b < 100; ++b)
+for (b = 0; b < 100; b++)
 {
 if (a < b)
 {
-putchar((a % 10) + 48);
 putchar((a / 10) + 48);
+putchar((a % 10) + 48);
 putchar(' ');
 putchar((b / 10) + 48);
 putchar((b % 10) + 48);
