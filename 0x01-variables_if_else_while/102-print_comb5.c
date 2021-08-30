8f01b41d41281174aa4d834a3c9d9fc2e6a42995
Moses Oyet David <trickmaxillar@ggmail.com>
2021-08-30 05:24:31 -0400

    Modifyfile

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,15 +10,15 @@ int main(void)
 int a, b;
 for (a = 0; a < 100; ++a)
 {
-for (b = 0; b < 100; b++)
+for (b = 0; b < 100; ++b)
 {
 if (a < b)
 {
 putchar((a % 10) + 48);
 putchar((a / 10) + 48);
 putchar(' ');
-putchar((b % 10) + 48);
 putchar((b / 10) + 48);
+putchar((b % 10) + 48);
 if ((a != 98) || (b != 98))
 {
 putchar(',');
