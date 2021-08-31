f226f15ea53f772019ef43995bbd7a41cac2c6ec
Moses Oyet David <trickmaxillar@ggmail.com>
2021-08-31 08:54:49 -0400

    Modify 0-holberton.h

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+*main - Contain function and loop
+*Return: return 0 success
+*/
+int main(void)
+{
+int i = 0;
+char c;
+char s[] = "Holberton\n";
+while (i <= 9)
+{
+c = s[9];
+putchar(c);
+i++;
+}
+return (0);
+}
