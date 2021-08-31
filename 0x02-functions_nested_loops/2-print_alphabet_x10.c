1041ebfd2977caa7090ec38c020a241d0c9f2c2a
Moses Oyet David <trickmaxillar@ggmail.com>
2021-08-31 10:58:09 -0400

    Modify file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - Entry point
+*Description: Contain nested while loop 
+*Return: void
+*/
+void print_alphabet_x10(void)
+{
+char x = 0, y;
+while (x <= 9)
+{
+y = 'a';
+while (y <= 'z')
+{
+_putchar(y);
+y++;
+}
+_putchar(x);
+X++;
+}
+_putchar('\n');
+}
