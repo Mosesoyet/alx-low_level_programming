43250ef30e5270050b95d1594ab32a1009069bd9
Moses Oyet David <trickmaxillar@ggmail.com>
2021-08-31 13:57:34 -0400

    Modify file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*add - should give sum of two integers
+*@a: integer being checked
+*@b: iteger being checked
+*Return: the sum of a and b
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
