7dce94e8dca8aeca9c6230b2d995e66b42ce3375
Moses Oyet David <trickmaxillar@ggmail.com>
2021-08-31 09:28:31 -0400

    Modify file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*main - contain statement and loop
+*Return: Always 0 success
+*/
+int main(void)
+{
+char x = 'a';
+do {
+_putchar(x);
+x++;
+}
+while (x <= 'z';);
+_putchar('\n');
+return (0);
+}
