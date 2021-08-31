cc33e7982f1be6d2f1b6c58ee09c6536bbbd82a5
Moses Oyet David <trickmaxillar@ggmail.com>
2021-08-31 12:07:10 -0400

    modify file

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  *@c: Charactor to exammine
  *Return: 1 if alphabet or 0 if not
  */
-int _isalpha(inc c)
+int _isalpha(int c)
 {
 if ((c >= 'a') || (c <= 'z') && (c >= 'A') || (c <= 'Z'))
 return (1);
