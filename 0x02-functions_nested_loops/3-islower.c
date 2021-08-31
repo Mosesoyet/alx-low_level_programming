c05c3035bca6057f611753ebedb5d96dc62437cd
Moses Oyet David <trickmaxillar@ggmail.com>
2021-08-31 11:44:59 -0400

    Modify file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_islower - entry point
+*Description: Contain nested loop
+*Return: always 1 else 0
+*/
+int _islower(int c);
+{
+  scantf("%c", &c);
+  c = getchar();
+  for (c = 'a'; c <= 'z'; c++)
+    {
+      if (_islower(c))
+	return (1);
+      else
+	return (0);
+    }
+}
