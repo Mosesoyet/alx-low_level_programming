7f0f7fafa18566e20e0a5737a24046401692cb23
Moses Oyet David <trickmaxillar@ggmail.com>
2021-08-31 11:23:31 -0400

    Modify file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,16 @@
 */
 void print_alphabet_x10(void)
 {
-char x = 0, y;
-while (x <= 9)
+char x;
+ int y = 0;
+while (y <= 9)
 {
-y = 'a';
-while (y <= 'z')
-{
-_putchar(y);
-y++;
-}
+x = 'a';
+while (x <= 'z')
+{ 
 _putchar(x);
 x++;
 }
+}
+_putchar('\n');
 }
