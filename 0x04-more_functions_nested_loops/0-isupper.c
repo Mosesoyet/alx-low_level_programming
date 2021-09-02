78f7f233a5eb5e3cc13e07bda638504e78578b8c
Moses Oyet David <trickmaxillar@ggmail.com>
2021-09-02 07:00:54 -0400

    modify file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
 * _isupper - Entry point
 * @c: Data type checked
@@ -7,7 +8,7 @@
 */
 int _isupper(int c)
 {
-if (c >= 'a' && c <= 'z')
+if (c >= 'A' && c <= 'Z')
 return (1);
 else
 return (0);
