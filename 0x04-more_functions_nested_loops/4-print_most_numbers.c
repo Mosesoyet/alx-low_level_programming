fc0352e4ed24914b976d3a85cb3715521608527e
Moses Oyet David <trickmaxillar@ggmail.com>
2021-09-02 10:15:37 -0400

    Modify file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 /**
 * print_most_numbers - Entry point
-* Description: A code to print digit 0 to 9 exlusive of 2 and 4
 * Return: void
 */
 void print_most_number(void)
@@ -11,9 +10,7 @@ char cx;
 for (cx = '0'; cx <= '9'; cx++)
 {
 if (!(cx == '2' || cx == '4'))
-
-_putchar((cx % 10) + '0');
-
+_putchar(cx);
 }
 _putchar('\n');
 }
