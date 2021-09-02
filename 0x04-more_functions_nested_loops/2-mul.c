14672f2ca21451a814ddbf855c85489e6b348a3c
Moses Oyet David <trickmaxillar@ggmail.com>
2021-09-02 04:30:42 -0400

    Modify file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* mul - Entry point
+* Desc: A program to multiply two integers
+* @a: Data type checked
+* @b: Data type
+* Return: result
+*/
+int mul(int a, int b)
+{
+int result;
+result = a * b;
+return (result);
+}
