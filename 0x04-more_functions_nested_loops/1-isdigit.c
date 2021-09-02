6907de550e8fc047ab3898d6065a4bff59bd78ef
Moses Oyet David <trickmaxillar@ggmail.com>
2021-09-02 04:22:12 -0400

    Modify file by adding text

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isdigit - Entry point
+* Desc: A program to check for digit
+* @c: Data type to check
+* Return: 1 if c is digit else 0
+*/
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+return (1);
+else
+return (0);
+}
