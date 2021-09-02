8e1c294f5f33284dc51649a7d4d888f5742f4bb3
Moses Oyet David <trickmaxillar@ggmail.com>
2021-09-02 06:21:19 -0400

    modify file

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_square - print square on screen
+* @size: The size of square
+* Return: void
+*/
+void print_square(int size)
+{
+int n, m;
+if (size <= 0)
+_putchar('\n');
+for (a = 0; a < size; a++)
+{
+for (b = 0; b < (size); b++);
+{
+putchar('#');
+}
+putchar('\n');
+}
+}
