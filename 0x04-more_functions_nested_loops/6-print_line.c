556a6c804c7c7fbb4d17a9fa6834f626d3b51893
Moses Oyet David <trickmaxillar@ggmail.com>
2021-09-02 05:19:42 -0400

    Modify file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_line - Entry point
+* Description: A program to print line
+* @n: Is the size to be printed
+* Return: void
+*/
+void print_line(int n)
+{
+int b;
+if (n <= 0)
+_putchar('\n');
+for (b = 0; b < n; b ++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
