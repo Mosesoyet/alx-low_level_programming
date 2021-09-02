7637daccad6a2d9e1a81d7529b6f8df2f4ac4fa3
Moses Oyet David <trickmaxillar@ggmail.com>
2021-09-02 10:10:32 -0400

    Modify file

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,14 @@
 #include <stdio.h>
 /**
 * print_numbers - Entry point
-* Description: A program to display digits 0 to 9
 * Return: void
 */
 void print_numbers(void)
 {
 char b;
-for (b = 48; b < 58; b++)
+for (b = '0'; b <= '9'; b++)
 {
-_putchar((b % 10) + '0');
+_putchar(b);
 }
 _putchar('\n');
 }
