9e55f14670496e9194425e3d65b1e9ebd4c77552
Moses Oyet David <trickmaxillar@ggmail.com>
2021-09-06 08:51:40 -0400

    Add code

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_rev - print string in reverse
+* @s: string to print
+* Return: void
+*/
+void print_rev(char *s)
+{
+int i = 0;
+while (*(s + i))
+i++;
+i = i - 1;
+while (i >= 0)
+{
+_puutchar(*(s + i));
+i--;
+}
+_putchar('\n');
+}
