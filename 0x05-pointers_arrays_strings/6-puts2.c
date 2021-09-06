b24547e80d15064c9a32c82f17ba1615e386ce62
Moses Oyet David <trickmaxillar@ggmail.com>
2021-09-06 09:09:33 -0400

    Modify file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* puts2 - print string charactor
+* @str: string to be print
+* Return: void
+*/
+void puts2(char *str)
+{
+int i = 0;
+while (*(s + i))
+{
+_putchar(*(str + i));
+i = i + 2;
+}
+_putchar('\n');
+}
