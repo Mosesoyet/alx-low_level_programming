85e50c46e986dca020b178df025525cd6a98826b
Moses Oyet David <trickmaxillar@ggmail.com>
2021-09-06 04:23:07 -0400

    Modify file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* swap_int - entry point for code
+* @a: data type to swap
+* @b: data type to swap
+* Return: void
+*/
+void swap_int(int *a, int *b)
+{
+int c;
+*a = c;
+*b = *a;
+c = *b;
+}
