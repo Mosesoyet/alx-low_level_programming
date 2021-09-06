5acf252176ec411c44179a3820aee331046d9d0a
Moses Oyet David <trickmaxillar@ggmail.com>
2021-09-06 05:02:30 -0400

    Modify file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _puts - it put the string on stdout
+* @str: the string to be printed
+* Return: nothing
+*/
+void _puts(char *str)
+{
+int j;
+while (*(str + j))
+{
+_puts(*(str + j));
+j++;
+}
+_puts('\n');
+}
