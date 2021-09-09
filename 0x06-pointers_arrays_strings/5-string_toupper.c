92fa48a1da7f094c9ea9202e2b30adb5df45e1fa
Moses Oyet David <trickmaxillar@gmail.com>
2021-09-09 17:40:07 +0300

    Add code

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,3 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 /**
+* string_toupper - print strings in upper case
+* @j: string to convert
+* Return: 0
+*/
+char *string_toupper(char *j)
+{
+int i = 0;
+while (*(j + i))
+{
+if (*(j + i) >= 'a' && *(j + i) <= 'z')
+*(j + i) -= 'a' - 'A';
+i++;
+}
+return (0);
+}
