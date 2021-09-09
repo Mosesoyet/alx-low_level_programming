2c51be117e4407255337bd319ffee94d1020dcc2
Moses Oyet David <trickmaxillar@gmail.com>
2021-09-09 17:15:25 +0300

    Fix error

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,16 +4,17 @@
 * reverse_array - print array in reverse
 * @a: integer to reverse
 * @n: an integer
-* Return: 0 succes
+* Return: void
 */
 void reverse_array(int *a, int n)
 {
-int i, k;
+int i = 0, k;
+n = n - 1;
 while (i < n)
 {
-t = *(a + i);
+k = *(a + i);
 *(a + i) = *(a + n);
-*(a + n) = t;
+*(a + n) = k;
 i++;
 n--;
 }
