9c2169af658e44ef49a0d48eb454309facca68e7
Moses Oyet David <trickmaxillar@gmail.com>
2021-09-13 18:52:19 +0300

    First modification

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,12 @@
 char *_strchr(char *s, char c)
 {
 int j;
-for (j = 0; s[j]; j++)
+for (j = 0; *(s + j); j++)
 {
-if (s[j] == c)
+if (*(s + j) == c)
 return (s + j);
 }
-if (s[j] == c)
+if (*(s + j) == c)
 return (s + j);
 return (0);
 }
