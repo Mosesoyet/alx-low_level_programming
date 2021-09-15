669c3d0655c2fe6626ae6f09ec5bd7153f5c9d3d
Moses Oyet David <trickmaxillar@gmail.com>
2021-09-15 15:50:36 +0300

    Add code

---
+++ 0x08-recursion/3-factoral.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* factorial - prints factor of a number
+* @n: number to to check
+* Return: the factorial of a given number
+*/
+int factorial(int n)
+{
+if (n < 0)
+return (-1);
+else if (n == 0)
+n = 1;
+factorial(n - 1);
+}
