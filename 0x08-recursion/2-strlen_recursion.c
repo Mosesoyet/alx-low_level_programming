68533aef962c3be5e1887d36eb1735e7de7e4f6a
Moses Oyet David <trickmaxillar@gmail.com>
2021-09-15 15:26:08 +0300

    Modify

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,16 +7,7 @@
 */
 int _strlen_recursion(int *s)
 {
-int i = 0;
-while (s[i])
-{
-i++;
-break;
-}
-if (s[i] != '\n')
-{
-_putchar(s[i]);
-_strlen_recursion = _strlen_recursion(s[i] - 1);
-}
-return (0);
+if (*s == '\0')
+return;
+_strlen_recursion(s - 1);
 }
