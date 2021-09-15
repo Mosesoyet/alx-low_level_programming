424ff10d6b29bf5bcf992a5015f13b46bd0a659b
Moses Oyet David <trickmaxillar@gmail.com>
2021-09-15 16:14:09 +0300

    Add code

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _pow_recursion - entry point
+* @x: data type,(value) to which y is raised
+* @y: power raised
+* Return: -1 if y is < 0 and return the value of x raised to the power of y
+*/
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+return (x**y * _pow_recursion(x**y - 1));
+}
