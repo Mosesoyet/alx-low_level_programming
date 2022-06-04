7028bf341db6e2b28c67bdeb8dbf66dd260bfd03
Moses Oyet David <trickmaxillar@gmail.com>
2022-06-04 17:59:39 +0300

    Fix code

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		return i is positive;
+	}
+	else if (i < 0)
+	{
+		return i is negative;
+	}
+	else
+	{
+		return i is 0;
+	}
+}
+
+int main(void)
+{
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <main.h>
+
+void positive_or_negative(int i);
+
+#endif /* By Moses Oyet for ALX */
