7039a5ffdc9ad381923242879dbde5b5404afef3
Moses Oyet David <trickmaxillar@gmail.com>
2022-06-14 08:40:21 +0300

    Fix the code

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,13 +7,16 @@
 * Return: 0
 */
 
-int main(void)
+void positive_or_negative()
 {
-        int i;
-	
+	int i;
 	srand(time(0));
 	i = rand() - RAND_MAX / 2;
-	positive_or_negative(i);
-
-        return (0);
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i);
+	return (0);
 }
